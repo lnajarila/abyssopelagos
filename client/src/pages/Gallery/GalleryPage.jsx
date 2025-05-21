6d1d38ea3a8e7a94cd6156babcb96394b68d22ff
Lurd Synell B. Najarila <lnajarila@gbox.adnu.edu.ph>
2025-05-21 23:45:46 +0800

    Implemented frontend routing, created page to view image in detail

---
+++ client/src/pages/Gallery/GalleryPage.jsx
@@ -0,0 +1,13 @@
+import ImageUpload from './ImageUpload.jsx';
+import ImageGrid from './ImageGrid.jsx';
+
+function GalleryPage() {
+    return (
+        <div>
+            <ImageUpload />
+            <ImageGrid />
+        </div>
+    );
+}
+
+export default GalleryPage;
